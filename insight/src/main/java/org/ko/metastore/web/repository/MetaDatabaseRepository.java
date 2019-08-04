8b3328f0acdefbd4bdf7c5ed9dc504ef4faf5bc1
uzies <ko.shen@hotmail.com>
2019-08-04 11:06:13 +0800

    #初始化

--- insight/src/main/java/org/ko/metastore/web/repository/MetaDatabaseRepository.java
+++
@@ -1,8 +0,0 @@
-package org.ko.metastore.web.repository;
-
-import org.ko.metastore.web.domain.MetaDatabase;
-import org.springframework.data.repository.CrudRepository;
-
-public interface MetaDatabaseRepository extends CrudRepository<MetaDatabase, Integer> {
-
-}
--- insight/src/main/scala/org/ko/metastore/web/repository/MetaTableRepository.scala
+++
@@ -1,8 +0,0 @@
-package org.ko.metastore.web.repository
-
-import org.ko.metastore.web.domain.MetaTable
-import org.springframework.data.repository.CrudRepository
-
-trait MetaTableRepository extends CrudRepository[MetaTable, Integer]{
-
-}
