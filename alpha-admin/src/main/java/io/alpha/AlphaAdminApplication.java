e43af25b8e308c90572dbccdfbaf77bd7ce1881f
uzies <ko.shen@hotmail.com>
2019-08-16 16:15:43 +0800

    #初始化

---
+++ alpha-admin/src/main/java/io/alpha/AlphaAdminApplication.java
@@ -0,0 +1,12 @@
+package io.alpha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlphaAdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AlphaAdminApplication.class, args);
+    }
+}
