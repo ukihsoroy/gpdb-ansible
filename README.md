1ae975dc4370747be52e6378786ceb744336414f
K.O <ko.shen@hotmail.com>
2019-12-25 15:33:40 +0800

    #modules

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # alpha-online
+
+---
+- `alpha-admin`: 运维管理
+- `alpha-app`: service模板
+- `alpha-auth`: 权限管理
+- `alpha-fusion`: 数据管理
+- `alpha-gateway`: 网关
---
+++ alpha-app/alpha-service/src/main/java/io/alpha/App.java
@@ -0,0 +1,13 @@
+package io.alpha;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
